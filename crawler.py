4386be9913279fda52adb75cb189ec4a42d9a94b
ironfactory8684 <44189189+ironfactory8684@users.noreply.github.com>
2021-07-25 23:25:54 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -169,7 +169,7 @@ def crawler(query, s_date, e_date, news_office, maxpage, sort, printed):
     
 def main():
     info_main = input("="*50+"\n"+"입력 형식에 맞게 입력해주세요."+"\n"+" 시작하시려면 Enter를 눌러주세요."+"\n"+"="*50)
-    maxpage = input("최대 크롤링할 페이지 수 입력하시오:"+"\n"+"최대 400페이지까지 가능합니다")
+    maxpage = input("최대 크롤링할 페이지 수 입력하시오:"+"\n"+"최대 400페이지까지 가능합니다"+"\n"+"만약 검색결과가 4000개를 넘으면 다시 실행됩니다")
     query = input("검색어 입력: ")
     sort = input("뉴스 검색 방식 입력(관련도순=0 최신순=1 오래된순=2): ") #관련도순=0 최신순=1 오래된순=2
     s_date = input("시작날짜 입력(2019.01.04):") #2019.01.04
